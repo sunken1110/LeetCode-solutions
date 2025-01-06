9a643988c5f40497346acd44decfb629257e4c8e
Seonggeun Kim <sunken1110@gmail.com>
2025-01-06 22:39:30 +0900

    Update solution

--- Solutions/M/1769-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ Solutions/M/1769-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -6,21 +6,24 @@
 
 # https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
 
+# Linear Scanning - Complexity O(n)
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
         ans = [0] * n
 
-        for dir in range(2):
-            total_ops = 0
-            partial_ops = 0
+        for direction in range(2):
+            total_ops = 0    # Cumulative sum of ops
+            partial_ops = 0    # Ops for ith box to (i+1)th box, where already 0 ~ (i-1)th boxes are moved to ith (left -> right case)
 
-            if dir == 0:
+            # left -> right scanning
+            if direction == 0:
                 for i in range(n):
                     ans[i] += total_ops
                     partial_ops += int(boxes[i])
                     total_ops += partial_ops
 
+            # right -> left scanning
             else:
                 for i in range(n-1, -1, -1):
                     ans[i] += total_ops
@@ -29,7 +32,7 @@ class Solution:
 
         return ans
 
-
+# Brute Force - Complexity O(n^2)
 class Solution2:
     def minOperations(self, boxes: str) -> List[int]:
         ans = [0] * len(boxes)
