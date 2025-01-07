4a06fa8f9190879827eb05f8a6681f7577cf91a3
Seonggeun Kim <sunken1110@gmail.com>
2025-01-08 08:16:40 +0900

    Update string-matching-in-an-array.py

--- Solutions/S/1408-string-matching-in-an-array/string-matching-in-an-array.py
+++ Solutions/S/1408-string-matching-in-an-array/string-matching-in-an-array.py
@@ -17,3 +17,17 @@ class Solution:
 
         return ans
 
+
+# Brute-force
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        n = len(words)
+        ans = []
+
+        for i in range(n):
+            for j in range(n):
+                if j != i and words[i] in words[j]:
+                    ans.append(words[i])
+                    break
+
+        return ans
