678c6b12cd73ddaf4b80642e36c5524ac7bca0eb
sunken1110 <sunken1110@gmail.com>
2025-04-04 14:06:07 +0900

    Daily

---
+++ Solutions/L/1123-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/description
+
+# Time Complexity O(N), Space Complexity O(H) where H is the height of the tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(tree, depth):
+            if not tree:
+                return (None, depth)
+
+            l_tree, l_depth = dfs(tree.left, depth+1)
+            r_tree, r_depth = dfs(tree.right, depth+1)
+
+            if l_depth > r_depth:
+                return (l_tree, l_depth)
+
+            elif l_depth < r_depth:
+                return (r_tree, r_depth)
+
+            else:
+                return (tree, l_depth)
+
+
+        return dfs(root, 0)[0]
